6c014495f0c6fe4330d7061b3b54c1ed85513759
Linyi (Charlie)  Peng <Charliepeng516@gmail.com>
2025-06-14 13:17:58 +1000

    Initial commit

---
+++ backend/src/controllers/auth.controller.js
@@ -0,0 +1,12 @@
+export const login = (req, res) => {
+    // Handle login logic here
+    res.send('Login endpoint');
+}
+export const signup = (req, res) => {
+    // Handle registration logic here
+    res.send('Register endpoint');
+}
+export const logout = (req, res) => {
+    // Handle logout logic here
+    res.send('Logout endpoint');
+}
---
+++ backend/src/index.js
@@ -0,0 +1,16 @@
+import express from 'express';
+import { connectDB } from './lib/db.js';
+import authRoutes from './routes/auth.route.js';
+import dotenv from 'dotenv';
+
+dotenv.config();
+const app = express();
+
+app.use("/api/auth", authRoutes);
+
+const PORT = process.env.PORT;
+
+app.listen(PORT, () => {
+  console.log(`Server is running on port is ${PORT}`);
+  connectDB();
+});
